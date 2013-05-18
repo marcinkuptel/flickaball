e9c0e590b3f15194687149cdbd923718d9204b7e
marcinkuptel <marcinkuptel@gmail.com>
2013-05-18 23:17:29 +0200

    Added unit tests

---
+++ Flick-a-ball tests/Flick_a_ball_tests.h
@@ -0,0 +1,13 @@
+//
+//  Flick_a_ball_tests.h
+//  Flick-a-ball tests
+//
+//  Created by Marcin Kuptel on 18/05/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Flick_a_ball_tests : SenTestCase
+
+@end
